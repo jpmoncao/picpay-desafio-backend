efb764118c36cdfd344a389e9fdc8e9ef8d53432
jpmoncao <pedrohjoao44@gmail.com>
2024-05-24 13:21:59 -0300

    feat: criando domain de transferencias

---
+++ src/database/domain/transferencia.ts
@@ -0,0 +1,7 @@
+export default interface TransferenciaProps {
+    id_transferencia?: number | null | undefined;
+    id_user?: number | null | undefined;
+    id_destinatario?: number | null | undefined;
+    valor?: number | null | undefined;
+    entrada_saida?: string | null | undefined;
+}
