36a0e2dbeb4d21467a4c6e342b7e1667a1c15a63
jpmoncao <pedrohjoao44@gmail.com>
2024-05-24 11:28:36 -0300

    feat: adicionando erros para cpf, cnpj e email duplicados

--- src/errors/User.ts
+++ src/errors/User.ts
@@ -11,7 +11,7 @@ export class UserNotFoundError extends Error {
     }
 }
 
-export class UserDuplicateUsernameError extends Error {
+export class UserDuplicateCNPJError extends Error {
     name: string;
     message: string;
     code: string;
@@ -19,8 +19,33 @@ export class UserDuplicateUsernameError extends Error {
     constructor(message: string) {
         super(message);
 
-        this.name = 'UserDuplicateUsernameError';
-        this.code = 'USERNAME_DUPLICATE'
+        this.name = 'UserDuplicateCNPJError';
+        this.code = 'USER_CNPJ_DUPLICATE'
+    }
+}
+
+export class UserDuplicateCPFError extends Error {
+    name: string;
+    message: string;
+    code: string;
+
+    constructor(message: string) {
+        super(message);
+
+        this.name = 'UserDuplicateCPFError';
+        this.code = 'USER_CPF_DUPLICATE'
+    }
+}
+export class UserDuplicateEmailError extends Error {
+    name: string;
+    message: string;
+    code: string;
+
+    constructor(message: string) {
+        super(message);
+
+        this.name = 'UserDuplicateEmailError';
+        this.code = 'USER_EMAIL_DUPLICATE'
     }
 }
 
