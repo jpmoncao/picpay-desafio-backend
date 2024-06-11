f36eeb7cd7fe1571b1db23347c8f3cd460d8e2a5
jpmoncao <pedrohjoao44@gmail.com>
2024-06-11 10:37:15 -0300

    feat: página para 404

--- src/server.ts
+++ src/server.ts
@@ -13,12 +13,13 @@ const app = express();
 app.use(cors({ exposedHeaders: ['X-Pagination', 'X-Pagination-Pages'] }));
 app.use(express.json());
 
+
 app.use('/users', new UserRouter().router);
 app.use('/shopkeepers', new ShopkeeperRouter().router);
 app.use('/wallets', new WalletRouter().router);
 app.use('/transfers', new TransferRouter().router);
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.status(202).json({
         "list-all-users": process.env.API_ADDRESS + '/user',
         "list-user": process.env.API_ADDRESS + '/user/{id}',
@@ -27,6 +28,19 @@ app.use('/', (req, res) => {
         "delete-user": process.env.API_ADDRESS + '/user/delete/{id}',
     })
 });
+
+app.all('*', (req, res) => {
+    res.status(404).json({
+        message: "Route not found!",
+        links: [
+            {
+                rel: 'home',
+                href: process.env.API_ADDRESS + '/',
+            }
+        ]
+    });
+});
+
 
 app.listen(process.env.API_PORT,
     () => console.log(`Server ping!\nRunning in port ${process.env.API_PORT}`)); 
